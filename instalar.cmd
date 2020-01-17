1321b0371848e897feae5e2129baaa875e3e118a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-01-17 09:52:09 -0300

    Update instalar.cmd

--- instalar.cmd
+++ instalar.cmd
@@ -1,1 +1,3 @@
-choco install %CD%\kubernetes-cli.1.17.0.nupkg %CD%\kubernetes-helm.2.14.3.nupkg %CD%\Minikube.1.6.2.nupkg
+choco install %CD%\kubernetes-cli.1.17.0.nupkg 
+choco install %CD%\kubernetes-helm.2.14.3.nupkg
+choco install %CD%\Minikube.1.6.2.nupkg
